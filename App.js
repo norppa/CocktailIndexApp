45d39b1baff53c8bd6091804f6f7c2952f10fd3b
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-30 12:24:28 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Blanc app</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default App;
